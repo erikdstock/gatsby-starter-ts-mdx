09ac3f33aa3f2bc748852784dd2ad70199937981
ErikS <erik@artsymail.com>
2019-02-25 09:39:31 -0500

    get site booting

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-// You can delete this file if you're not using it
+const { introspectionQuery, graphql, printSchema } = require("gatsby/graphql")
 
 
 /**
