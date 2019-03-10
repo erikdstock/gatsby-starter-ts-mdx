47fe7648c9cf6d0dcb15d8e35e4ea5167d1301ab
Erik Stockmeier <erik.d.stock@gmail.com>
2019-03-10 12:17:55 -0400

    Query markdown files with remark

---
+++ content/pages/about.md
@@ -0,0 +1,8 @@
+---
+collectionName: pages
+path: /about
+date: 2017-07-12T17:12:33.962Z
+title: Welcome to my little blog
+---
+
+This is the website ![](/assets/cat.jpg)
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,80 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const { introspectionQuery, graphql, printSchema } = require("gatsby/graphql")
+const { createFilePath } = require("gatsby-source-filesystem")
 const write = require("write")
+const path = require("path")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  // Add path to markdown nodes
+  if (node.internal.type === "MarkdownRemark") {
+    let nodePath = node.frontmatter.path
+
+    // if there is a path in frontmatter, use it
+    if (!nodePath) {
+      const subPath = `/${node.frontmatter.collectionName || "/writing"}`
+      nodePath = path.join(
+        subPath,
+        createFilePath({
+          node,
+          getNode,
+          basePath: "content/blog", // for now all content is coming from here
+          trailingSlash: false,
+        })
+      )
+    }
+    // console.log("\ncreating slug field", nodePath)
+    createNodeField({
+      name: `path`,
+      node,
+      value: nodePath,
+    })
+  }
+}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const markdownDefaultTemplate = path.resolve(
+    `src/layouts/MarkdownDefault.tsx`
+  )
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            id
+            excerpt
+            frontmatter {
+              path
+              collectionName
+            }
+            fields {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      // console.log(`creating node at ${node.fields.slug} - ${node.id}`)
+      createPage({
+        path: node.fields.path,
+        component: markdownDefaultTemplate,
+        context: {}, // additional data can be passed via context
+      })
+    })
+  })
+}
 
 /**
  * Generate GraphQL schema.json file to be read by tslint
