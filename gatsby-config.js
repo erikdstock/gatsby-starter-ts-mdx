0319428680edbb0765d7b16738a9666dec09a863
Erik Stockmeier <erik.d.stock@gmail.com>
2019-03-11 09:53:12 -0400

    set up font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -177,7 +177,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Fira Mono", "Karma:400,700", "Lato:400,700"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
